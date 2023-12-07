a5de681fee936548a4ca0f67dc8ba835c3e6b84b
Borodinets <assassqwas@gmail.com>
2023-12-08 02:14:58 +0500

    2.4

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.assertj;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkEmptyArr() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Names array is empty");
+    }
+
+    @Test
+    void checkMissSym() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key:value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key:value does not contain the symbol '='");
+    }
+
+    @Test
+    void checkMissKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=value does not contain a key");
+    }
+
+    @Test
+    void checkMissVal() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key= does not contain a value");
+    }
+
+}
