04d221399172d528975a295fb2ccdcdac70ac5b2
Borodinets <assassqwas@gmail.com>
2023-12-08 22:39:41 +0500

    2. Итератор для двухмерного массива int[][] [#9539]

--- src/test/java/ru/job4j/assertj/SimpleConvertTest.java
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -3,7 +3,10 @@ package ru.job4j.assertj;
 import org.assertj.core.data.Index;
 import org.junit.jupiter.api.Test;
 
-import java.util. *;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
