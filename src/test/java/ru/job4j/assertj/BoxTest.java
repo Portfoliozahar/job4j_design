dea8d1ce0d29c9671b07bc5e0e0e2b318e7de3fc
Borodinets <assassqwas@gmail.com>
2023-12-09 00:21:26 +0500

    123

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -7,30 +7,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BoxTest {
 
     @Test
-    void isSphere() {
-        Box box = new Box(0, 10);
-        assertThat(box.whatsThis()).isEqualTo("Sphere");
-        assertThat(box.getNumberOfVertices()).isEqualTo(0);
-        assertThat(box.isExist()).isTrue();
-        assertThat(box.getArea()).isEqualTo(4 * Math.PI * (10 * 10));
+    void testWhatsThis() {
+        Box box1 = new Box(0, 10);
+        Box box2 = new Box(4, 10);
+        assertThat(box1.whatsThis()).isEqualTo("Sphere");
+        assertThat(box2.whatsThis()).isEqualTo("Tetrahedron");
     }
 
     @Test
-    void isCube() {
-        Box box = new Box(8, 7);
-        assertThat(box.whatsThis()).isEqualTo("Cube");
-        assertThat(box.getNumberOfVertices()).isEqualTo(8);
-        assertThat(box.isExist()).isTrue();
-        assertThat(box.getArea()).isEqualTo(6 * (7 * 7));
+    void testGetNumberOfVertices() {
+        Box box1 = new Box(0, 10);
+        Box box2 = new Box(4, 10);
+        assertThat(box1.getNumberOfVertices()).isEqualTo(0);
+        assertThat(box2.getNumberOfVertices()).isEqualTo(4);
     }
 
     @Test
-    void isTetrahedron() {
-        Box box = new Box(4, 5);
-        assertThat(box.whatsThis()).isEqualTo("Tetrahedron");
-        assertThat(box.getNumberOfVertices()).isEqualTo(4);
-        assertThat(box.isExist()).isTrue();
-        assertThat(box.getArea()).isEqualTo(Math.sqrt(3) * (5 * 5));
+    void testIsExist() {
+        Box box1 = new Box(0, 10);
+        Box box2 = new Box(4, 0);
+        assertThat(box1.isExist()).isTrue();
+        assertThat(box2.isExist()).isFalse();
     }
 
+    @Test
+    void testGetArea() {
+        Box box1 = new Box(0, 5);
+        Box box2 = new Box(4, 10);
+        assertThat(box1.getArea()).isEqualTo(4 * Math.PI * (5 * 5));
+        assertThat(box2.getArea()).isEqualTo(Math.sqrt(3) * (10 * 10));
+    }
 }
